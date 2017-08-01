0a8975f1ea2c2b3279a1baa235a5f99c12d484bf
oleh02 <oleh02@gmail.com>
2017-08-01 18:30:47 +0300

    db init

---
+++ SchoolDashboard/DAL/Scripts/CreateDb.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE SchoolLevels
+(
+  Id			INT	PRIMARY KEY	NOT NULL,
+  Description	TEXT			NOT NULL
+)
+
+CREATE TABLE Lessons
+(
+  Id			INT PRIMARY KEY	NOT NULL,
+  SchoolLevel	INT				NOT NULL,
+  StartTime		TEXT			NOT NULL,
+  EndTime		TEXT			NOT NULL,
+  FOREIGN KEY (SchoolLevel) REFERENCES SchoolLevels(Id)
+)
